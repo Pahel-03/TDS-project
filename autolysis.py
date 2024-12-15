01972bfbc3b411982a9609bd588042914f4f9dce
Pahel-03 <22f3000853@ds.study.iitm.ac.in>
2024-12-15 17:59:41 +0530

    Autolysis final check

--- autolysis.py
+++ autolysis.py
@@ -29,37 +29,35 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
-#Assign value to API_KEY
+#Assign value to API_KEY as environment variable
 API_KEY=os.environ["AIPROXY_TOKEN"]
+
 #Extract filename from stdin
 filename=sys.argv[1]
 
-# Load the CSV file
+# Load the CSV file using pandas
 def load_data(name):
-    ldata = pd.read_csv(name,encoding_errors="replace")
-    return ldata
+    loaded_data = pd.read_csv(name,encoding_errors="replace")
+    return loaded_data
 
-data=load_data(filename)
-
-# Descriptive Statistics of the data
+# Descriptive Statistics of the data using describe()
 def make_decriptive_stats(data):
     summary=data.describe(include="all").to_markdown()
     return(summary)
 
-
-# Drop rows with any null values
+# Drop rows with any null values using dropna()
 def drop_null_rows(data):
     df_dropped_rows = data.dropna()
     return df_dropped_rows
 
-# Correlation map of numerical columns
+# Correlation map of numerical columns using corr()
 def make_correl_map(data):
     categorical_columns_with_bool = data.select_dtypes(include=['object', 'category', 'bool']).columns
     num_data=data.drop(categorical_columns_with_bool,axis=1)
     correlation_matrix_data=num_data.corr()
     return correlation_matrix_data.to_json()
 
-#Call AI to generate a correlation heatmap
+#Call AI to generate a correlation heatmap from a generated correlation map
 def heatmap_code(statistics):
     response = requests.post(
             "http://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
@@ -75,15 +73,15 @@ def heatmap_code(statistics):
                 ]
             }
         )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response
 
-#correaltion Analysis using ai-using vision capabilities
+#Correaltion Analysis using ai-using vision capabilities of the heatmap generated
 def correl_analysis(correl_mat):
     response = requests.post(
             "http://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
@@ -96,15 +94,15 @@ def correl_analysis(correl_mat):
                 ]
             }
         )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response
 
-#Generate Feature importance output
+#Generate Feature importances of all features using PCA and ranking them
 def give_feature_importances(data):
     categorical_columns = data.select_dtypes(include=['object']).columns
     for col in categorical_columns:
@@ -127,16 +125,15 @@ def give_feature_importances(data):
                 ]
             }
         )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response
 
-#AI call for Data description
-first_row_of_data=data.iloc[0].to_json()
+#AI call for Data description-brief description based on the column names and first row values
 def describe_data(first_row):
     response = requests.post(
             "http://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
@@ -150,15 +147,16 @@ def describe_data(first_row):
                 ]
             }
         )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response
 
-# redudant features / elimination - openai function calling
+# Deleting redundant columns using AI intuition - openai function calling implemented
+# Statement to be passed for function calling generated
 def is_it_redundant(first_row):
     response = requests.post(
             "http://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
@@ -173,14 +171,15 @@ def is_it_redundant(first_row):
                 # "functions":function_descriptions # specify the function call
             }
         )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response
 
+#Drop the columns based on column names passed using function call
 def drop_red_cols(resp,drop_it):
 # Split the matched strings by comma to get column names
     column_names = []
@@ -193,6 +192,7 @@ def drop_red_cols(resp,drop_it):
         redun_resp="The dataset is a well defined one with no redundant columns."
     return redun_resp,column_names
 
+#Implementing function call to delete redundant columns
 def create_red_explain(statement):    
     function_descriptions = [
     {
@@ -228,19 +228,18 @@ def create_red_explain(statement):
                 "function_call":"auto"# specify the function call
             }
         )
-    datar=response.json()
-    message = datar['choices'][0]['message']
+    data_by_response=response.json()
+    message = data_by_response['choices'][0]['message']
     if 'function_call' in message:
         function_call=message['function_call']
         arguments = json.loads(function_call['arguments'])  # Parse the arguments as JSON
-        resp_redun,drop_l=drop_red_cols(arguments['resp'],arguments['drop_it'])
-    # print(arguments)
+        response_redundant,drop_list=drop_red_cols(arguments['resp'],arguments['drop_it'])
     else:
-        datar=None
-        resp_redun,drop_l=drop_red_cols("No",[])
-    return resp_redun,datar,drop_l
+        data_by_response=None
+        response_redundant,drop_list=drop_red_cols("No",[])
+    return response_redundant,data_by_response,drop_list
 
-#drop redundant columns
+#Drop columns - general Function
 def drop_cols(datan,drop):
     if drop==[]:
         return datan
@@ -261,15 +260,15 @@ def basic_desc(statistics):
                 ]
             }
         )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response
 
-#AI call for visualising analysis done
+#AI call for visualising analysis done-Generate Code for saving plots
 def further_analysis(statistics):
     response = requests.post(
             "http://aiproxy.sanand.workers.dev/openai/v1/chat/completions",
@@ -288,14 +287,13 @@ def further_analysis(statistics):
                 ]
             }
         )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response
 
 #Extract python code from gpt reponse
 def extract_python_code(json_data):
@@ -317,6 +315,7 @@ def execute_extracted_code(code):
     except Exception as e:
         print(f"Error during code execution: {e}")
 
+#Provide vision analysis on the figures saved locally
 def image_process(image_url):
     # Function to encode the image
     def encode_image(image_path):
@@ -336,71 +335,73 @@ def image_process(image_url):
                     ]
                     }
                     )
-    datar=response.json()
-    choices_content = [choice['message']['content'] for choice in datar.get('choices', [])]
-    sresp=""
+    data_by_response=response.json()
+    choices_content = [choice['message']['content'] for choice in data_by_response.get('choices', [])]
+    string_response=""
     # Print the extracted content
     for index, content in enumerate(choices_content):
-        sresp+=(f"\n{content}\n")
-    return sresp,datar,image_url
+        string_response+=(f"\n{content}\n")
+    return string_response,data_by_response,image_url
 
 #Run analysis functions on dataset
 
-first_row_response,token_4=describe_data(first_row_of_data)
-redundant_response,token_5=is_it_redundant(first_row_of_data)   
-# print(redundant_response)
-is_it,lists,drop_list=create_red_explain(redundant_response)
-data=drop_cols(data,drop_list)
-data=data.dropna()
-correl_response,token_8=correl_analysis(make_correl_map(data))
-feature_imp,token_9=give_feature_importances(data)
-summary_response,token_1=basic_desc(make_decriptive_stats(data))
-analyse_response,token_2=further_analysis(make_decriptive_stats(data))
-code_1=extract_python_code(token_2)
-execute_extracted_code(code_1)
-heatmap_mat_code,token_3=heatmap_code(make_correl_map(data))
-code_2=extract_python_code(token_3)
-execute_extracted_code(code_2)
+if __name__ == '__main__':
+    #Load the dataset
+    data=load_data(filename)
+    #Get basic description of data
+    first_row_response,token_1=describe_data(data.iloc[0].to_json())
+    #Check for redundant columns and create reponse
+    redundant_response,token_2=is_it_redundant(data.iloc[0].to_json())   
+    is_it,lists,drop_list=create_red_explain(redundant_response)
+    data=drop_cols(data,drop_list)
+    data=drop_null_rows(data)
+    #Generate response for correlation analysis 
+    correl_response,token_3=correl_analysis(make_correl_map(data))
+    # Generate response for feature importance
+    feature_imp,token_4=give_feature_importances(data)
+    #generate Response for Descriptive Statistics
+    summary_response,token_5=basic_desc(make_decriptive_stats(data))
+    #Generate code for vision analysis
+    analyse_response,token_6=further_analysis(make_decriptive_stats(data))
+    code_1=extract_python_code(token_6)
+    execute_extracted_code(code_1)
+    heatmap_mat_code,token_7=heatmap_code(make_correl_map(data))
+    code_2=extract_python_code(token_3)
+    execute_extracted_code(code_2)
 
-# GET IMAGE PATH 
-current_directory = "./" 
+    #Code to get image urls and titles for readme.md titles
+    current_directory = "./" 
+    png_urls = []
+    for filename in os.listdir(current_directory):
+        if filename.endswith(".png"):
+            file_path = os.path.join(current_directory, filename)
+            url = os.path.relpath(file_path, "./").replace("\\", "/")
+            png_urls.append(url)
+    image_text,image_tokens,order_titles=[],[],[]
+    for url in png_urls:
+        image_resp,img_token,title=image_process(url)
+        image_text.append(image_resp)
+        image_tokens.append(img_token)
+        order_titles.append(title)
 
-# List to store URLs of PNG files
-png_urls = []
+    #Generate README.md
+    with open("README.md", "w") as f:
+        f.write("## An introduction to the data ##\n\n")
+        f.write(str(first_row_response))
+        f.write("\n\n## Asking ai to indentify redundant columns in our dataset ##\n\n")
+        f.write(str(is_it))
+        f.write("\n\n##  Descriptive Statistics of the dataset ##\n")
+        f.write(str(make_decriptive_stats(data)))
+        f.write("\n\n## Understanding Descriptive Statistics ##\n")
+        f.write(str(summary_response))
+        f.write("\n\n## How important are our columns - Feature importance Analysis based on PCA ##\n\n")
+        f.write(str(feature_imp))
+        f.write("\n## How are our columns related to each other? - Correlation Analysis ##\n")
+        f.write(str(correl_response))
+        f.write("\n\n## What do the charts entail about our columns? - Using vision capabilities to analyse them ##\n\n")
+        for i in range(len(image_text)):
+            f.write(f'## {order_titles[i]} ##\n')
+            f.write(str(image_text[i])+'\n\n')
 
-# Loop through all files in the current directory
-for filename in os.listdir(current_directory):
-    if filename.endswith(".png"):
-        file_path = os.path.join(current_directory, filename)
-        # Construct the URL for each PNG file
-        url = os.path.relpath(file_path, "./").replace("\\", "/")
-        png_urls.append(url)
-
-image_text,image_tokens,order_titles=[],[],[]
-for url in png_urls:
-    image_resp,img_token,title=image_process(url)
-    image_text.append(image_resp)
-    image_tokens.append(img_token)
-    order_titles.append(title)
-
-
-with open("README.md", "w") as f:
-    f.write("## An introduction to the data ##\n\n")
-    f.write(str(first_row_response))
-    f.write("\n\n## Asking ai to indentify redundant columns in our dataset ##\n\n")
-    f.write(str(is_it))
-    f.write("\n\n##  Descriptive Statistics of the dataset ##\n")
-    f.write(str(make_decriptive_stats(data)))
-    f.write("\n\n## Understanding Descriptive Statistics ##\n")
-    f.write(str(summary_response))
-    f.write("\n\n## How important are our columns - Feature importance Analysis based on PCA ##\n\n")
-    f.write(str(feature_imp))
-    f.write("\n## How are our columns related to each other? - Correlation Analysis ##\n")
-    f.write(str(correl_response))
-    f.write("\n\n## What do the charts entail about our columns? - Using vision capabilities to analyse them ##\n\n")
-    for i in range(len(image_text)):
-        f.write(f'## {order_titles[i]} ##\n')
-        f.write(str(image_text[i])+'\n\n')
-
-print("Analysis complete. See README.md for details.")
+    print("Analysis complete. See README.md for details.")
 
